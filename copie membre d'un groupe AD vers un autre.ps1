c7f54bf41501971375b42feb3680900d06dbd134
Raphael-Digonnaux <septar77@gmail.com>
2026-04-28 09:59:49 +0200

    Create copie membre d'un groupe AD vers un autre.ps1

---
+++ copie membre d'un groupe AD vers un autre.ps1
@@ -0,0 +1,11 @@
+﻿# Remplacez ces variables avec les noms de vos groupes
+$distributionGroup = "nom_groupe_source"
+$securityGroup = "nom_groupe_cible"
+
+# Récupérer les membres du groupe de distribution
+$members = Get-ADGroupMember -Identity $distributionGroup
+
+# Ajouter chaque membre au groupe de sécurité
+foreach ($member in $members) {
+    Add-ADGroupMember -Identity $securityGroup -Members $member
+}
